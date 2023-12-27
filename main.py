591a0011b59b015fe8a77c477266d5a18842e4f7
pimpochkina <mcvarvar@ya.ru>
2023-12-27 18:23:05 +0300

    final update

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import sqlite3
 import sys
 
 from PyQt5 import uic
-from PyQt5.QtWidgets import QStackedWidget, QApplication, QWidget, QMainWindow, QLineEdit
+from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QTableWidgetItem
 from PyQt5.QtCore import Qt
 from random import *
 
@@ -70,6 +70,7 @@ class CreatingAccForm(QWidget):
         if self.check_info.isChecked():
             self.name = self.name_input.text()
             if self.name:
+                # добавляем нового пользователя в базу данных
                 self.cur.execute("""INSERT INTO userdata(login, password, name) VALUES(?, ?, ?)""",
                                  (self.login, self.password, self.name))
                 self.con.commit()
@@ -78,7 +79,7 @@ class CreatingAccForm(QWidget):
                 self.main_window = MainWindow(self.name)
                 self.main_window.show()
 
-
+# создание класса главного окна приложения
 class MainWindow(QMainWindow):
     def __init__(self, name):
         super().__init__()
@@ -93,6 +94,7 @@ class MainWindow(QMainWindow):
         self.lower_recipe_names = [w[0].lower() for w in self.recipe_names]
         self.ings = []
         self.mainInit()
+    # метод поиска рецепта по названию
 
     def name_search(self):
         self.found_recipe_list.clear()
@@ -104,8 +106,10 @@ class MainWindow(QMainWindow):
             for e in result:
                 self.found_recipe_list.addItem(e[0])
             self.found_label.setText(f'Найдено рецептов: {len(result)}')
+        else:
+            self.found_label.setText(f'Найдено рецептов: 0')
 
-
+    # метод поиска рецепта по ингредиенту
     def search_ings(self):
         self.found_recipe_list.clear()
         ing = self.ing_search_edit.text()
@@ -115,7 +119,10 @@ class MainWindow(QMainWindow):
                 for e in result:
                     self.found_recipe_list.addItem(e[0])
                 self.found_label.setText(f'Найдено рецептов: {len(result)}')
+            else:
+                self.found_label.setText(f'Найдено рецептов: 0')
 
+    # метод поиска рецептов по тегам
     def tags_search(self):
         self.found_recipe_list.clear()
         tags = []
@@ -147,16 +154,22 @@ class MainWindow(QMainWindow):
             tags.append('легкое,')
         if self.diet_box.isChecked():
             tags.append('диетическое,')
-        tags_request = " AND ".join([f"tags LIKE '%{tag}%'" for tag in tags])
-        result = self.cur_recipes.execute(f"""SELECT name FROM recipes WHERE {tags_request}""").fetchall()
-        if len(result) != 0:
-            for e in result:
-                self.found_recipe_list.addItem(e[0])
-            self.found_label.setText(f'Найдено рецептов: {len(result)}')
+        if len(tags) != 0:
+            tags_request = " AND ".join([f"tags LIKE '%{tag}%'" for tag in tags])
+            result = self.cur_recipes.execute(f"""SELECT name FROM recipes WHERE {tags_request}""").fetchall()
+            if len(result) != 0:
+                for e in result:
+                    self.found_recipe_list.addItem(e[0])
+                self.found_label.setText(f'Найдено рецептов: {len(result)}')
+            else:
+                self.found_label.setText(f'Найдено рецептов: 0')
 
     def clear_search(self):
         self.found_recipe_list.clear()
         self.found_label.setText(f'Найдено рецептов: 0')
+
+    def read_searched_recipe(self, item):
+        self.read_recipe(item.text())
 
     def search_pageInit(self):
         self.stackedWidget.setCurrentIndex(1)
@@ -164,7 +177,7 @@ class MainWindow(QMainWindow):
         self.search_ings_btn.clicked.connect(self.search_ings)
         self.search_tags_btn.clicked.connect(self.tags_search)
         self.clear_search_btn.clicked.connect(self.clear_search)
-        self.found_recipe_list.itemClicked.connect(self.read_recipe(item))
+        self.found_recipe_list.itemClicked.connect(self.read_searched_recipe)
 
     def return_on_mainpg(self):
         self.stackedWidget.setCurrentIndex(0)
@@ -184,8 +197,6 @@ class MainWindow(QMainWindow):
         self.tip_label.setAlignment(Qt.AlignCenter)
         self.tip_label.setText(self.tips[self.tip_cur_index])
 
-        # pass
-
     def next_tip(self):
         if self.tip_cur_index + 1 >= len(self.tips):
             self.tip_cur_index = 0
@@ -193,8 +204,27 @@ class MainWindow(QMainWindow):
             self.tip_cur_index += 1
         self.tip_label.setText(self.tips[self.tip_cur_index])
 
-    def read_recipe(self):
-        pass
+    def read_recipe(self, name):
+        self.stackedWidget.setCurrentIndex(2)
+        request = self.cur_recipes.execute(f"""SELECT * FROM recipes WHERE name = ?""", (name, )).fetchall()
+        self.recipe_name_label.setText(name)
+        self.recipe_label.setText(request[0][4])
+        ing_list = request[0][3].split(',')
+        tags_list = request[0][2].split(',')
+        self.ings_table.setRowCount(len(ing_list))
+        output_tags = ', '.join(tags_list)
+        self.tags_label.setText(f'Теги: {output_tags}')
+        row_index = 0
+        self.servings.setText(str(request[0][7]))
+        self.ccalories.setText(str(request[0][5]))
+        self.time.setText(str(request[0][6]))
+        for e in ing_list:
+            index = e.find('(')
+            item1 = QTableWidgetItem(e[:index])
+            item2 = QTableWidgetItem(e[index+1:-1])
+            self.ings_table.setItem(row_index, 0, item1)
+            self.ings_table.setItem(row_index, 1, item2)
+            row_index += 1
 
     def previous_tip(self):
         if self.tip_cur_index == 0:
@@ -212,18 +242,21 @@ class MainWindow(QMainWindow):
         self.today_calories = 0
         self.today_ccal.display(self.today_calories)
 
+    def read_random_recipe(self):
+        name = self.random_recipe_label.text()
+        self.read_recipe(name)
+
     def mainInit(self):
         self.search_page_btn.clicked.connect(self.search_pageInit)
         self.main_page_btn.clicked.connect(self.return_on_mainpg)
         self.next_random_btn.clicked.connect(self.set_random_recipe)
         self.set_random_recipe()
-        self.read_btn.clicked.connect(self.read_recipe)
+        self.read_btn.clicked.connect(self.read_random_recipe)
         self.set_random_tips()
         self.next_tip_btn.clicked.connect(self.next_tip)
         self.previous_tip_btn.clicked.connect(self.previous_tip)
         self.add_dish_btn.clicked.connect(self.add_ccal)
         self.clear_today_ccal.clicked.connect(self.clear_ccal)
-
 
 
 
